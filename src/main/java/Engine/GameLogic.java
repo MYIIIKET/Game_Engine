7e08f7ae29faead2424e323c6ba5ce57fbe955b9
Евгений Сидоренко <mylllket@yandex.ua>
2017-05-24 12:27:50 +0300

    GameLogic interface created

---
+++ src/main/java/Engine/GameLogic.java
@@ -0,0 +1,12 @@
+package Engine;
+
+
+public interface GameLogic {
+    void init() throws Exception;
+
+    void input(Window window);
+
+    void update(float interval);
+
+    void render(Window window);
+}
